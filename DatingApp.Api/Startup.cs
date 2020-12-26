4afde2dbee7192f4285732015b3656a647dca1af
Nico Nel <niconel@gmail.com>
2020-12-26 13:40:56 +0200

    Walking Skeleton with CORS and SSL

--- DatingApp.Api/Startup.cs
+++ DatingApp.Api/Startup.cs
@@ -25,6 +25,7 @@ namespace DatingApp.Api
                 options.UseSqlServer(_config.GetConnectionString("DefaultConnection"));
             });
             services.AddControllers();
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,6 +36,8 @@ namespace DatingApp.Api
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
             app.UseAuthorization();
 
